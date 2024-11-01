c13560382337627abf0ad4952410ee779a6ccfff
Giovanny <148973221+nbagiovanny@users.noreply.github.com>
2024-11-01 08:24:15 -0300

    .

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ function criarCartao(pergunta, repota){
 
     cartao.innerHTML= `
   <div class="cartao--conteudo">
-            <div class="cartao--pergunta"> <p>Qual a influência do oceano no clima no planeta?</p></div>
-            <div class="cartao--resposta"><p>
-                O clima da Terra representa a distribuição da energia recebida do Sol, em forma de radiação, ao longo da superfície esférica terrestre. O equador recebe mais radiação do que os pólos, e a atmosfera e o oceano são os responsáveis por reduzir esse gradiente térmico, transportando energia em forma de calor das baixas para as altas latitudes e tornando o clima globalmente mais homogêneo.</p></div>
+            <div class="cartao--pergunta"> <p>${pergunta}</p></div>
+            <div class="cartao--resposta"> <p>${repota}</p></div>
                 </div>
     `
 
